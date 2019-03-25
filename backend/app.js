40593c7f0915a04a3f1dbba28c8e9a09a685ce0d
NikitaSysoev <na.sysoev@gmail.com>
2019-03-25 11:33:57 +0300

    add eslint and prettier

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,45 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const cors = require("cors");
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
 
 // MongoDB database url
-const uri =
-  "mongodb+srv://admin:g4C4fPPCTFFBqic@cluster0-wyalb.mongodb.net/test?retryWrites=true";
+const uri = 'mongodb+srv://admin:g4C4fPPCTFFBqic@cluster0-wyalb.mongodb.net/test?retryWrites=true';
 
 // connects our back end code with the database
 mongoose.connect(uri, { useNewUrlParser: true });
 
 let db = mongoose.connection;
 // в случае успешного подключения выводим сообщение
-db.once("open", () => console.log("connected to the database"));
+db.once('open', () => console.log('connected to the database'));
 // в случае неудачного подключения выводим ошибку
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(cors());
 
-router.get("/hello", (req, res) => {
-  return res.send("hello");
+router.get('/hello', (req, res) => {
+  return res.send('hello');
 });
 
 // append /api for our http requests
-app.use("/api", router);
+app.use('/api', router);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  useEffect(()=>{
-    axios('http://localhost:3001/api/hello').then(res=>setData(res.data));
+  useEffect(() => {
+    axios('http://localhost:3001/api/hello').then(res => setData(res.data));
   });
-  return (
-    <div className="App">
-      getting {data ? data : '...'}
-    </div>
-  );
+  return <div className="App">getting {data ? data : '...'}</div>;
 };
 
 export default App;
